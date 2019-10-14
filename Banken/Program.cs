48d5345814f5d7eea03068a1d915740200800016
mathkarl <mathkarl@LSE-B2725>
2019-10-14 11:41:52 +0200

    Fixa alla kommenteringar och try/catch method

--- Banken/Program.cs
+++ Banken/Program.cs
@@ -83,6 +83,10 @@ namespace Banken
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// WriteFile skriver ner ändringar som har gjorts i list
+        /// </summary>
+        /// <param name="args"></param>
         private static void WriteFile(string filepath, string filename)
         {
             string f = filepath + filename;
@@ -104,6 +108,10 @@ namespace Banken
             File.AppendAllText(f, appendText);
         }
 
+        /// <summary>
+        /// ReadFile läser in alla namn som har lagts in i banken och lägger de i en file
+        /// </summary>
+        /// <param name="args"></param>
         private static void ReadFile(string filepath, string filename)
         {
             string f = filepath + filename; //Objectet f letar efter filepath och filename för vars Customer kommer ligga i
@@ -119,6 +127,10 @@ namespace Banken
             }
         }
 
+        /// <summary>
+        /// ShowCustomer visar alla Customer tillsammans med Balance i list i nummer ordning
+        /// </summary>
+        /// <param name="args"></param>
         static void ShowCustomer()
         {
             foreach (Customer i in list)
@@ -127,6 +139,10 @@ namespace Banken
             }
         }
 
+        /// <summary>
+        /// RemoveCustomer används för att ta bort en Customer som finns i list
+        /// </summary>
+        /// <param name="args"></param>
         static void RemoveCustomer()
         {
             int n = 1;
@@ -136,19 +152,49 @@ namespace Banken
                 n += 1;
             }
             Console.WriteLine("Vilken använder vill du ta bort? ");
-            int remove = int.Parse(Console.ReadLine()); //Objectet remove används för att ta bort en användare från list i Customer
+            int remove = 0;
+            try
+            {
+                remove = int.Parse(Console.ReadLine()); //Objectet remove används för att ta bort en användare från list i Customer som hade skrivits ut av WriteLine 
+            }
+            catch
+            {
+                Console.WriteLine("Ett fel uppstod");
+            }
             list.RemoveAt(remove - 1);
 
         }
 
+        /// <summary>
+        /// AddCustomer används för att lägga in en ny Customer och Balance till list
+        /// </summary>
+        /// <param name="args"></param>
         static void AddCustomer()
         {
             Customer info1 = new Customer(); //Objectet info1 skapas av klassen Customer med hjälp av konstruktorn
-            info1.Name = Console.ReadLine();
-            info1.Balance = int.Parse(Console.ReadLine());
+            try
+            {
+                info1.Name = Console.ReadLine(); //Instansen info1
+            }
+            catch
+            {
+                Console.WriteLine("Ett fel uppstod");
+            }
+            try
+            {
+                info1.Balance = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Ett fel uppstod");
+            }
             list.Add(info1);
         }
 
+        /// <summary>
+        /// ShowBalance används för att se hur mycket en Customer har i Balance
+        /// </summary>
+        /// <param name="args"></param>
         static void ShowBalance()
         {
             int n = 1;
@@ -158,10 +204,22 @@ namespace Banken
                 n += 1;
             }
             Console.WriteLine("Vilken använder vill du see? ");
-            int show = int.Parse(Console.ReadLine()); //Objectet show frågar efter någon i klassen Customer
+            int show = 0;
+            try
+            {
+                show = int.Parse(Console.ReadLine()); //Objectet show frågar efter någon i klassen Customer
+            }
+            catch
+            {
+                Console.WriteLine("Ett fel uppstod");
+            }
             Console.WriteLine(list[show].ShowCustomer);
         }
 
+        /// <summary>
+        /// AddBalance används för att lägga in Balance till en Customer som finns i list
+        /// </summary>
+        /// <param name="args"></param>
         static void AddBalance()
         {
             int n = 1;
@@ -171,13 +229,33 @@ namespace Banken
                 n += 1;
             }
             Console.WriteLine("Vilken använder ska du sätta pengar i? ");
-            int customer = int.Parse(Console.ReadLine()); //Objectet customer vill veta vilken i klassen Customer ska få pengar
+            int customer = 0;
+            try
+            {
+                customer = int.Parse(Console.ReadLine()); //Objectet customer vill veta vilken i klassen Customer ska få pengar
+            }
+            catch
+            {
+                Console.WriteLine("Ett fel uppstod");
+            }
             Console.WriteLine("Hur mycket ska du ge? ");
-            int money = int.Parse(Console.ReadLine()); //Objectet money frågar hur mycket pengar du ska ge till personen i klassen Customer
+            int money = 0;
+            try
+            {
+                money = int.Parse(Console.ReadLine()); //Objectet money frågar hur mycket pengar du ska ge till personen i klassen Customer
+            }
+            catch
+            {
+                Console.WriteLine("Ett fel uppstod");
+            }
             list[customer].Balance += money;
 
         }
 
+        /// <summary>
+        /// RemoveBalance används för att ta bort Balance från en Customer som finns i list
+        /// </summary>
+        /// <param name="args"></param>
         static void RemoveBalance()
         {
             int n = 1;
@@ -187,12 +265,32 @@ namespace Banken
                 n += 1;
             }
             Console.WriteLine("Vilken använder ska du ta pengar ifrån? ");
-            int customer = int.Parse(Console.ReadLine()); //Objectet customer vill veta vilken i klassen Customer som ska förlora pengar
+            int customer = 0;
+            try
+            {
+                customer = int.Parse(Console.ReadLine()); //Objectet customer vill veta vilken i klassen Customer som ska förlora pengar
+            }
+            catch
+            {
+                Console.WriteLine("Ett fel uppstod");
+            }
             Console.WriteLine("Hur mycket ska du ta? ");
-            int money = int.Parse(Console.ReadLine()); //Objectet money frågar hur mycket du ska ta ifrån personen i klassen Customer
+            int money = 0;
+            try
+            {
+                money = int.Parse(Console.ReadLine()); //Objectet money frågar hur mycket du ska ta ifrån personen i klassen Customer
+            }
+            catch
+            {
+                Console.WriteLine("Ett fel uppstod");
+            }
             list[customer].Balance -= money;
         }
 
+        /// <summary>
+        /// SelectMenuItem är vad du välja att göra i banken med 1 till 7
+        /// </summary>
+        /// <param name="args"></param>
         static int SelectMenuItem()
         {
             Console.WriteLine("Välkommen till banken!");
